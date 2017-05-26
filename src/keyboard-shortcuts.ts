0a41a272df6cc0727341c2878f1cea24917f2681
Jan Kuri <jkuri88@gmail.com>
2017-05-27 00:10:52 +0200

    chore(): win fixes updates

--- src/keyboard-shortcuts.ts
+++ src/keyboard-shortcuts.ts
@@ -15,7 +15,6 @@ export let keyboardShortcuts = [
   {keypress: 'CommandOrControl+9', sctype: 'send', sckey: 'switchTab', scvalue: 8 },
   {keypress: 'CommandOrControl+0', sctype: 'send', sckey: 'switchTab', scvalue: 9 },
   {keypress: 'CommandOrControl+V', sctype: 'send', sckey: 'paste', scvalue: null },
-  {keypress: 'CommandOrControl+C', sctype: 'send', sckey: 'copy', scvalue: null },
   {keypress: 'CommandOrControl+N', sctype: 'createWindow', sckey: 'createWindow', scvalue: null },
   {keypress: 'CommandOrControl+Shift+I', sctype: 'toggleDevTools', sckey: 'toggleDevTools', scvalue: null }
 ]
