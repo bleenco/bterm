056049ed0023e7c2775251afb3ade927cd174d83
Jan Kuri <jkuri88@gmail.com>
2017-05-29 11:59:54 +0200

    build(dist): fix dist build

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -50,6 +50,6 @@ module.exports = {
     'system': '{}',
     'file': '{}',
     'node-pty': 'require("node-pty")',
-    "font-manager": '"font-manager"'
+    "font-manager": 'require("font-manager")'
   }
 };
